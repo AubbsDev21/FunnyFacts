49ed7f799357bb064c3bb53e6784210b15979f64
Aubre O. Body <navyaubbs32@gmail.com>
2016-02-01 21:03:33 -0500

    Completed app for now

--- app/src/main/java/com/aubbsapps/funnyfacts/MainMenuActivity.java
+++ app/src/main/java/com/aubbsapps/funnyfacts/MainMenuActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 public class MainMenuActivity extends Activity {
     protected Button mButtonOne;
     protected Button mButtonTwo;
+    protected Button mButtonThree;
+    protected Button mButtonFour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class MainMenuActivity extends Activity {
 
         mButtonOne = (Button)findViewById(R.id.Aniamal_button);
         mButtonTwo = (Button)findViewById(R.id.Sportsbutton);
+        mButtonThree = (Button)findViewById(R.id.Moviebutton);
+        mButtonFour = (Button)findViewById(R.id.Randombutton);
 
         /*-----------------   Menu buttons that intent to their classes------------ */
         mButtonOne.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +37,26 @@ public class MainMenuActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainMenuActivity.this, CoolFactsActivity2.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        mButtonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, CoolFactsActivity3.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+        mButtonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, CoolFactsActivity4.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
